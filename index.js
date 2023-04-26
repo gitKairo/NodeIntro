07cf515f5363885ca73f5f5471d42c3bdb594aad
RUGE <kr0937619@dt038576.otc.edu>
2023-04-26 13:07:12 -0500

    added API_KEY

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ import chalk from 'chalk';
 import axios from 'axios';
 import scripts from './scripts.js';
 import { configs } from './scripts.js';
+import {config} from 'dotenv';
+import { env } from 'process';
 
 scripts();
 configs();
+console.log(process.env.API_KEY);
 
-axios.get( "https://dog.ceo/api/breeds/image/random Fetch!"
-)
+let api_url = `
+https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.API_KEY}&language=en-US&page=1
+`
+
+
+
+axios.get(api_url)
+.then (res => {
+    console.log(res.data)
+})
